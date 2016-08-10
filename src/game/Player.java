57b68e1ba76505bd5bc763570141d2d374155a1a
Ashish Baghudana <ashish.baghudana26@gmail.com>
2016-08-10 15:50:15 +0530

    Create abstract class Player

---
+++ src/game/Player.java
@@ -0,0 +1,10 @@
+package game;
+
+import structures.Hand;
+
+public abstract class Player {
+	public static String[] playerNames = {"Player 1", "Computer"};
+	
+	public String name;
+	protected Hand hand;
+}
